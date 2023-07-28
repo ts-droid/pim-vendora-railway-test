7a25ed91ef4aeb0a2da0fb6e5407f18216bf1548
Anton Kihlström <anton@scriptsector.se>
2023-07-28 09:41:31 +0200

    Typo fix

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
+use App\Events\InventoryReceiptUpdated;
 use App\Listeners\UpdateArticleExternalCost;
-use App\Models\InventoryReceipt;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -20,7 +20,7 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
-        InventoryReceipt::class => [
+        InventoryReceiptUpdated::class => [
             UpdateArticleExternalCost::class,
         ],
     ];
