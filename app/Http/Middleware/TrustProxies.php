cecd72b4383abab0cecba5dcd2d4dd875160a343
Anton Kihlström <anton@scriptsector.se>
2023-11-06 15:16:50 +0100

    Add load balancer as trusted proxy

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,9 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies;
+    protected $proxies = [
+        '142.93.136.204'
+    ];
 
     /**
      * The headers that should be used to detect proxies.
