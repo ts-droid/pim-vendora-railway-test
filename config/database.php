a1fd4178b3605cafca140abd6c1d8bac60bcf6c2
Anton Kihlström <anton@scriptsector.se>
2023-11-20 12:48:53 +0100

    Add support for SSH tunnel

--- config/database.php
+++ config/database.php
@@ -65,22 +65,16 @@ return [
 
         'mysql_prod' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST_PROD', '127.0.0.1'),
-            'port' => env('DB_PORT_PROD', '3306'),
-            'database' => env('DB_DATABASE_PROD', 'forge'),
-            'username' => env('DB_USERNAME_PROD', 'forge'),
-            'password' => env('DB_PASSWORD_PROD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'strict' => true,
-            'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+            'host'      => env('TUNNELER_LOCAL_ADDRESS'),
+            'port'      => env('TUNNELER_LOCAL_PORT'),
+            'database'  => env('DB_DATABASE'),
+            'username'  => env('DB_USERNAME'),
+            'password'  => env('DB_PASSWORD'),
+            'charset'   => env('DB_CHARSET', 'utf8mb4'),
+            'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
+            'prefix'    => env('DB_PREFIX', ''),
+            'timezone'  => env('DB_TIMEZONE', '+00:00'),
+            'strict'    => env('DB_STRICT_MODE', true),
         ],
 
         'pgsql' => [
