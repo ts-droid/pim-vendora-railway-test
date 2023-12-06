4ffdca76648daa170a163efc7f616d3271d0626c
Anton Kihlström <anton@scriptsector.se>
2023-12-06 12:26:38 +0100

    Fix for getStockTime

--- app/Services/ArticleQuantityCalculator.php
+++ app/Services/ArticleQuantityCalculator.php
@@ -164,6 +164,6 @@ class ArticleQuantityCalculator
             return 0;
         }
 
-        return round($salesPerMonth / $netStock, 1);
+        return round($netStock / $salesPerMonth, 1);
     }
 }
