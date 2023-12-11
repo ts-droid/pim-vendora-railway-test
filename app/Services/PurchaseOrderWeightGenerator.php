cc5d6423e8f40e80730879fffc603b1c206ff702
Anton Kihlström <anton@scriptsector.se>
2023-12-11 09:06:22 +0100

    Fix for missing historical data

--- app/Services/PurchaseOrderWeightGenerator.php
+++ app/Services/PurchaseOrderWeightGenerator.php
@@ -16,6 +16,10 @@ class PurchaseOrderWeightGenerator
 
         $average = array_sum($this->quantityPerMonth) / count($this->quantityPerMonth);
 
+        if (!$average) {
+            return;
+        }
+
         $configs = [];
         for ($i = 1;$i <= 12;$i++) {
             $configs['purchase_system_weight_auto_' . $i] = round($this->quantityPerMonth[$i] / $average, 2);
@@ -32,11 +36,15 @@ class PurchaseOrderWeightGenerator
     {
         $year = date('Y', strtotime('-1 year'));
 
+        if ($year <= 2022) {
+            $year = 2023;
+        }
+
         for ($i = 1;$i <= 12;$i++) {
             $startDate = $year . '-' . $i . '-01';
             $endDate = $year . '-' . $i . '-' . date('t', strtotime($startDate));
 
-            $this->quantityPerMonth[$i] = (int) DB::table('sales_order_lines')
+            $this->quantityPerMonth[$i] = DB::table('sales_order_lines')
                 ->join('sales_orders', 'sales_orders.id', '=', 'sales_order_lines.sales_order_id')
                 ->whereBetween('sales_orders.date', [$startDate, $endDate])
                 ->sum('sales_order_lines.quantity');
