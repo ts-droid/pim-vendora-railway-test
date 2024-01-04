23fe8c61ede0c02913ade5a966503df9562f75bb
Anton Kihlström <anton@scriptsector.se>
2024-01-04 10:25:28 +0100

    Log remote order

--- app/Services/VismaNet/VismaNetPurchaseOrderService.php
+++ app/Services/VismaNet/VismaNetPurchaseOrderService.php
@@ -83,6 +83,8 @@ class VismaNetPurchaseOrderService extends VismaNetApiService
         // Fetch purchase order from Visma.net so that we can detect changes
         $remoteOrder = $this->callAPI('GET', '/v1/purchaseorder/' . $purchaseOrder->order_number);
 
+        log_data('Remote purchase order: ' . json_encode($remoteOrder));
+
         if (empty($remoteOrder['orderNbr'])) {
             return ['success' => false, 'message' => 'Remote order could not be found.'];
         }
