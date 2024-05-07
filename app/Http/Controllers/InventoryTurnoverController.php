2d42d74020e3cdd58c1b213602caf017b692d9ab
Anton Kihlström <anton@scriptsector.se>
2024-05-07 10:18:55 +0200

    Show turnover rate

--- app/Http/Controllers/InventoryTurnoverController.php
+++ app/Http/Controllers/InventoryTurnoverController.php
@@ -10,8 +10,22 @@ class InventoryTurnoverController extends Controller
 {
     public function index(Request $request)
     {
-        $period = (int) $request->input('period', 6);
+        $period = (int) $request->input('period', 3);
 
+        $lastPeriodStartDate = date('Y-m-d', strtotime('-' . ($period * 2) . ' months'));
+        $startDate = date('Y-m-d', strtotime('-' . $period . ' months'));
+        $endDate = date('Y-m-d');
+
+        // Fetch all order lines within the period
+        $orderLines = DB::table('sales_order_lines')
+            ->join('sales_orders', 'sales_order_lines.sales_order_id', '=', 'sales_orders.id')
+            ->select('article_number', 'quantity', 'sales_orders.date')
+            ->whereBetween('sales_orders.date', [$lastPeriodStartDate, $endDate])
+            ->get()
+            ->groupBy('article_number')
+            ->toArray();
+
+        // Fetch all articles
         $articles = DB::table('articles')
             ->select('id', 'article_number', 'description', 'stock', 'cost_price_avg', 'external_cost')
             ->get();
@@ -20,6 +34,24 @@ class InventoryTurnoverController extends Controller
             foreach ($articles as &$article) {
                 $article->cost_price = $article->cost_price_avg ?: $article->external_cost;
                 $article->stock_value = $article->stock * $article->cost_price;
+
+                $article->stock_turnover_rate = 0;
+                $article->stock_turnover_rate_last_period = 0;
+
+                if (isset($orderLines[$article->article_number])) {
+                    foreach ($orderLines[$article->article_number] as $orderLine) {
+                        if ($orderLine->date >= $startDate) {
+                            $article->stock_turnover_rate += $orderLine->quantity;
+                        }
+
+                        if ($orderLine->date >= $lastPeriodStartDate) {
+                            $article->stock_turnover_rate_last_period += $orderLine->quantity;
+                        }
+                    }
+                }
+
+                $article->stock_turnover_rate = $article->stock_turnover_rate / $period;
+                $article->stock_turnover_rate_last_period = $article->stock_turnover_rate_last_period / $period;
             }
         }
 
