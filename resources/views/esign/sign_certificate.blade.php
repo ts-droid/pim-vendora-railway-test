5b6218888f5b967de326f01536e509406fc78198
Anton Kihlström <anton@scriptsector.se>
2024-06-09 10:51:10 +0200

    Remove debug for loop

--- resources/views/esign/sign_certificate.blade.php
+++ resources/views/esign/sign_certificate.blade.php
@@ -27,14 +27,14 @@ $base64Badge = 'data:image/png;base64,' . base64_encode(file_get_contents($badge
     <body>
         <div style="font-family: Arial, Helvetica, sans-serif;">
             <h1 style="font-size: 20px;">Signing Parties</h1>
-            @for ($i = 0;$i < 3;$i++)
-                <div style="background-color: #F3F4F6;padding: 10px;font-size: 10px;margin-bottom: 15px;">
-                    <div style="font-weight: bold;margin-bottom: 5px;">Signed by {{ $document->recipient_name }}</div>
-                    <div style="color: #575757;">Signed at: {{ $document->signed_at }}</div>
-                    <div style="color: #575757;">IP: {{ $document->sign_ip }}</div>
-                    <div style="color: #575757;">User-agent: {{ $document->sign_user_agent }}</div>
-                </div>
-            @endfor
+
+            <div style="background-color: #F3F4F6;padding: 10px;font-size: 10px;margin-bottom: 15px;">
+                <div style="font-weight: bold;margin-bottom: 5px;">Signed by {{ $document->recipient_name }}</div>
+                <div style="color: #575757;">Signed at: {{ $document->signed_at }}</div>
+                <div style="color: #575757;">IP: {{ $document->sign_ip }}</div>
+                <div style="color: #575757;">User-agent: {{ $document->sign_user_agent }}</div>
+            </div>
+
             <div style="font-size: 10px;margin-top: 20px;color: #575757;">
                 This document has been electronically signed in accordance with the Electronic Signatures in Global and National Commerce Act (ESIGN)
                 and the Uniform Electronic Transactions Act (UETA). By affixing their electronic signature, the signatory acknowledges and agrees
