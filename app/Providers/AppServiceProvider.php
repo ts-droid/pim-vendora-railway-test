16abb35eb4898a1ae8a1d31174dcb72df9692b2d
Anton Kihlström <anton@scriptsector.se>
2024-06-14 11:11:31 +0200

    Allow access by IP

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
 
@@ -23,5 +24,11 @@ class AppServiceProvider extends ServiceProvider
         if ($this->app->environment('production')) {
             URL::forceScheme('https');
         }
+
+        Gate::define('viewPulse', function() {
+            return in_array(get_user_ip(), [
+                '95.143.200.80'
+            ]);
+        });
     }
 }
