550a7693342b118fcd754f22f04afb19fff1ceb2
Anton Kihlström <anton@scriptsector.se>
2024-06-20 08:22:14 +0200

    Add command black list

--- app/Providers/CommandDurationServiceProvider.php
+++ app/Providers/CommandDurationServiceProvider.php
@@ -13,6 +13,12 @@ class CommandDurationServiceProvider extends ServiceProvider
     protected $startTimes = [];
     protected $threshold = 60;
 
+    protected $blackList = [
+        'queue:work',
+        'pulse:check',
+        'schedule:run'
+    ];
+
     /**
      * Register services.
      */
@@ -27,10 +33,18 @@ class CommandDurationServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Event::listen(CommandStarting::class, function (CommandStarting $event) {
+            if (in_array($event->command, $this->blackList)) {
+                return;
+            }
+
             $this->startTimes[$event->command] = microtime(true);
         });
 
         Event::listen(CommandFinished::class, function(CommandFinished $event) {
+            if (in_array($event->command, $this->blackList)) {
+                return;
+            }
+
             if (isset($this->startTimes[$event->command])) {
                 $duration = microtime(true) - $this->startTimes[$event->command];
 
