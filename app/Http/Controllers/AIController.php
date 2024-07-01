0492480ffb9f4e1978a74280e86188c55d7924e2
Anton Kihlström <anton@scriptsector.se>
2024-07-01 14:39:28 +0200

    Log error

--- app/Http/Controllers/AIController.php
+++ app/Http/Controllers/AIController.php
@@ -38,6 +38,8 @@ class AIController extends Controller
             curl_setopt($ch, CURLOPT_POST, true);
             curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($streamData['body']));
             curl_setopt($ch, CURLOPT_WRITEFUNCTION, function($ch, $data) {
+                error_log('Raw response: ' . $data);
+
                 echo $data;
                 if (ob_get_length() !== false) {
                     ob_flush();
