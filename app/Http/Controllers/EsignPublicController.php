15d46b8c926ff0f10329a16a9845625f45f2eb80
Anton Kihlström <anton@scriptsector.se>
2024-07-24 13:43:04 +0200

    Change header

--- app/Http/Controllers/EsignPublicController.php
+++ app/Http/Controllers/EsignPublicController.php
@@ -14,9 +14,11 @@ class EsignPublicController extends Controller
             abort(404);
         }
 
+        $filename = makeFilenameFriendly($document->name . '_signed') . '.pdf';
+
         return response($document->getDocumentContent())
             ->header('Content-Type', 'application/pdf')
-            ->header('Content-Disposition', 'inline; filename="document.pdf"')
+            ->header('Content-Disposition', 'attachment; filename="' . $filename . '"')
             ->header('Cache-Control', 'no-cache, must-revalidate')
             ->header('Pragma', 'no-cache')
             ->header('Expires', '0');
