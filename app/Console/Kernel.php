355580edad32df44e9ce38f2773c347f8d5d40fb
Anton Kihlström <anton@scriptsector.se>
2024-10-23 08:14:47 +0200

    Tempporay disable cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,7 +28,7 @@ class Kernel extends ConsoleKernel
             $schedule->command('visma:process-queue')->everyMinute()->withoutOverlapping();
 
             // Visma.net
-            $schedule->command('visma:fetch quick')->everyFiveMinutes();
+            //$schedule->command('visma:fetch quick')->everyFiveMinutes();
             $schedule->command('visma:fetch daily')->dailyAt('02:00');
             $schedule->command('visma:fetch hourly')->hourly();
             $schedule->command('process-visma-deletions')->dailyAt('04:00');
