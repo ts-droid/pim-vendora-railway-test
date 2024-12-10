e1ba07d65aba5c9e4196fefd604f61b7824c0d2e
Anton Kihlström <anton@scriptsector.se>
2024-12-10 12:22:07 +0100

    Fix infinite loop

--- app/Services/WMS/StockOptimizationManager.php
+++ app/Services/WMS/StockOptimizationManager.php
@@ -42,111 +42,104 @@ class StockOptimizationManager
 
                 $articleVolume = ($article->height / 1000) * ($article->width / 1000) * ($article->depth / 1000);
 
-                // Loop until all stock for this article has been managed
-                while($stockData['managedStock'] < $stockData['stock']) {
-                    // Start looking at the stock places with the same classification, and continue downwards
-                    for ($i = $classIndex;$i < count(self::CLASSIFICATION_ORDER);$i++) {
-                        $stockPlaceClass = self::CLASSIFICATION_ORDER[$i];
-                        $stockPlaces = $groupedStockPlaces[self::CLASSIFICATION_ORDER[$i]] ?? [];
+                // Start looking at the stock places with the same classification, and continue downwards
+                for ($i = $classIndex;$i < count(self::CLASSIFICATION_ORDER);$i++) {
+                    $stockPlaceClass = self::CLASSIFICATION_ORDER[$i];
+                    $stockPlaces = $groupedStockPlaces[self::CLASSIFICATION_ORDER[$i]] ?? [];
 
-                        if (!$stockPlaces) continue;
+                    if (!$stockPlaces) continue;
 
-                        if ($stockData['has_main_placement']
-                            && ($stockPlaceClass == 'A' || $stockPlaceClass == 'B')) {
-                            continue;
-                        }
+                    if ($stockData['has_main_placement']
+                        && ($stockPlaceClass == 'A' || $stockPlaceClass == 'B')) {
+                        continue;
+                    }
 
-                        // First look for existing placements
-                        foreach ($stockPlaces as $stockPlace) {
-                            foreach ($stockPlace->compartments as $compartment) {
-                                foreach($compartment->stockItems as $stockItem) {
-                                    if ($stockItem->article_number != $article->article_number) continue;
+                    // First look for existing placements
+                    foreach ($stockPlaces as $stockPlace) {
+                        foreach ($stockPlace->compartments as $compartment) {
+                            foreach($compartment->stockItems as $stockItem) {
+                                if ($stockItem->article_number != $article->article_number) continue;
 
-                                    $stockData['managedStock']++;
-                                }
-                            }
-                        }
-
-                        // Look for refills
-                        foreach ($stockPlaces as $stockPlace) {
-                            foreach ($stockPlace->compartments as $compartment) {
-                                $stockItemCount = $compartment->stockItems->count();
-
-                                if (!$stockItemCount) continue; // Empty stock place
-
-                                $articleNumber = $compartment->stockItems->first()->article_number;
-                                if ($articleNumber != $article->article_number) continue; // Another article is hosted in this compartment
-
-                                // This article is hosted here, check if it is time to refill
-                                $compartmentVolume = ($compartment->height / 100) * ($compartment->width / 100) * ($compartment->depth / 100);
-                                $occupiedVolume = $articleVolume * $stockItemCount;
-                                $freeVolume = ($compartmentVolume * self::MAX_FILL) - $occupiedVolume;
-
-                                if ($occupiedVolume > self::REFILL_THRESHOLD) continue; // No need to refill, volume is not below 40%
-
-                                // Refill the compartment
-                                $refillCount = floor($freeVolume / $articleVolume);
-                                $refillCount = min($refillCount, ($stockData['stock'] - $stockData['managedStock']));
-
-                                if (!$refillCount) continue; // Not items found to refill with
-
-                                // Make a stock movement
-                                $this->makeStockMovement(
-                                    $article->article_number,
-                                    0,
-                                    $compartment->id,
-                                    intval($refillCount)
-                                );
-
-                                $stockData['managedStock'] += $refillCount;
-
-                                if ($stockPlaceClass == 'A' || $stockPlaceClass == 'B') {
-                                    $stockData['has_main_placement'] = true;
-                                    continue 3; // Move to next article
-                                }
-                            }
-                        }
-
-                        // Fill remaining stock to new compartments
-                        foreach ($stockPlaces as $stockPlace) {
-                            foreach ($stockPlace->compartments as $compartment) {
-                                if ($compartment->stockItems->count()) continue; // Compartment is not empty
-
-                                $compartmentCache = $this->movementCache[$compartment->id] ?? null;
-                                if ($compartmentCache && $compartmentCache['article_number'] != $article->article_number) {
-                                    // Another article is planed to moved to this compartment
-                                    continue;
-                                }
-
-                                $compartmentVolume = ($compartment->height / 100) * ($compartment->width / 100) * ($compartment->depth / 100);
-                                $occupiedVolume = $articleVolume * ($compartmentCache['quantity'] ?? 0);
-                                $freeVolume = ($compartmentVolume * self::MAX_FILL) - $occupiedVolume;
-
-                                $fillCount = floor($freeVolume / $articleVolume);
-                                $fillCount = min($fillCount, ($stockData['stock'] - $stockData['managedStock']));
-
-                                if (!$fillCount) continue;
-
-                                // Make a stock movement
-                                $this->makeStockMovement(
-                                    $article->article_number,
-                                    0,
-                                    $compartment->id,
-                                    intval($fillCount)
-                                );
-
-                                $stockData['managedStock'] += $fillCount;
-
-                                if ($stockPlaceClass == 'A' || $stockPlaceClass == 'B') {
-                                    $stockData['has_main_placement'] = true;
-                                    continue 3; // Move to next article
-                                }
+                                $stockData['managedStock']++;
                             }
                         }
                     }
 
-                    if ($stockData['managedStock'] >= $stockData['stock']) {
-                        break;
+                    // Look for refills
+                    foreach ($stockPlaces as $stockPlace) {
+                        foreach ($stockPlace->compartments as $compartment) {
+                            $stockItemCount = $compartment->stockItems->count();
+
+                            if (!$stockItemCount) continue; // Empty stock place
+
+                            $articleNumber = $compartment->stockItems->first()->article_number;
+                            if ($articleNumber != $article->article_number) continue; // Another article is hosted in this compartment
+
+                            // This article is hosted here, check if it is time to refill
+                            $compartmentVolume = ($compartment->height / 100) * ($compartment->width / 100) * ($compartment->depth / 100);
+                            $occupiedVolume = $articleVolume * $stockItemCount;
+                            $freeVolume = ($compartmentVolume * self::MAX_FILL) - $occupiedVolume;
+
+                            if ($occupiedVolume > self::REFILL_THRESHOLD) continue; // No need to refill, volume is not below 40%
+
+                            // Refill the compartment
+                            $refillCount = floor($freeVolume / $articleVolume);
+                            $refillCount = min($refillCount, ($stockData['stock'] - $stockData['managedStock']));
+
+                            if (!$refillCount) continue; // Not items found to refill with
+
+                            // Make a stock movement
+                            $this->makeStockMovement(
+                                $article->article_number,
+                                0,
+                                $compartment->id,
+                                intval($refillCount)
+                            );
+
+                            $stockData['managedStock'] += $refillCount;
+
+                            if ($stockPlaceClass == 'A' || $stockPlaceClass == 'B') {
+                                $stockData['has_main_placement'] = true;
+                                continue 3; // Move to next article
+                            }
+                        }
+                    }
+
+                    // Fill remaining stock to new compartments
+                    foreach ($stockPlaces as $stockPlace) {
+                        foreach ($stockPlace->compartments as $compartment) {
+                            if ($compartment->stockItems->count()) continue; // Compartment is not empty
+
+                            $compartmentCache = $this->movementCache[$compartment->id] ?? null;
+                            if ($compartmentCache && $compartmentCache['article_number'] != $article->article_number) {
+                                // Another article is planed to moved to this compartment
+                                continue;
+                            }
+
+                            $compartmentVolume = ($compartment->height / 100) * ($compartment->width / 100) * ($compartment->depth / 100);
+                            $occupiedVolume = $articleVolume * ($compartmentCache['quantity'] ?? 0);
+                            $freeVolume = ($compartmentVolume * self::MAX_FILL) - $occupiedVolume;
+
+                            $fillCount = floor($freeVolume / $articleVolume);
+                            $fillCount = min($fillCount, ($stockData['stock'] - $stockData['managedStock']));
+
+                            if (!$fillCount) continue;
+
+                            // Make a stock movement
+                            $this->makeStockMovement(
+                                $article->article_number,
+                                0,
+                                $compartment->id,
+                                intval($fillCount)
+                            );
+
+                            $stockData['managedStock'] += $fillCount;
+
+                            if ($stockPlaceClass == 'A' || $stockPlaceClass == 'B') {
+                                $stockData['has_main_placement'] = true;
+                                continue 3; // Move to next article
+                            }
+                        }
                     }
                 }
             }
