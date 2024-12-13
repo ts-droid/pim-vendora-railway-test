bf584de75f96f8c7011da3e6764f6dfe52abdd47
Anton Kihlström <anton@scriptsector.se>
2024-12-13 08:22:48 +0100

    Use configs

--- app/Console/Commands/ClassifyArticles.php
+++ app/Console/Commands/ClassifyArticles.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Http\Controllers\ConfigController;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 
@@ -21,15 +22,31 @@ class ClassifyArticles extends Command
      */
     protected $description = 'Classify articles based on their sales data';
 
+    private $config;
+
     /**
      * Execute the console command.
      */
     public function handle()
     {
+        $this->loadConfig();
         $this->classifySalesVolume();
         $this->classifyVolume();
 
         $this->info('Updated article classifications');
+    }
+
+    private function loadConfig()
+    {
+        $this->config = [
+            'article_sales_class_size_a' => (int) ConfigController::getConfig('article_sales_class_size_a', 0),
+            'article_sales_class_size_b' => (int) ConfigController::getConfig('article_sales_class_size_b', 0),
+            'article_sales_class_history' => (int) ConfigController::getConfig('article_sales_class_history', 0),
+            'article_sales_class_unique_invoices' => (int) ConfigController::getConfig('article_sales_class_unique_invoices', 0),
+            'sales_class_calculation' => (string) ConfigController::getConfig('sales_class_calculation', 'sales'),
+            'article_volume_class_breakpoint_a' => (float) ConfigController::getConfig('article_volume_class_breakpoint_a', 0),
+            'article_volume_class_breakpoint_b' => (float) ConfigController::getConfig('article_volume_class_breakpoint_b', 0)
+        ];
     }
 
     private function classifyVolume()
@@ -45,10 +62,10 @@ class ClassifyArticles extends Command
                 $classification = '';
 
                 if ($volume) {
-                    if ($volume < 0.001) {
+                    if ($volume <= $this->config['article_volume_class_breakpoint_a']) {
                         $classification = 'A';
                     }
-                    else if ($volume < 0.005) {
+                    else if ($volume <= $this->config['article_volume_class_breakpoint_b']) {
                         $classification = 'B';
                     }
                     else {
@@ -73,7 +90,7 @@ class ClassifyArticles extends Command
                 ->join('customer_invoices', 'customer_invoices.id', '=', 'customer_invoice_lines.customer_invoice_id')
                 ->selectRaw('SUM(customer_invoice_lines.quantity) as total_quantity, COUNT(DISTINCT customer_invoice_lines.customer_invoice_id) as unique_invoices')
                 ->where('customer_invoice_lines.article_number', '=', $articleNumber)
-                ->where('customer_invoices.date', '>=', date('Y-m-d H:i:s', strtotime('-120 days')))
+                ->where('customer_invoices.date', '>=', date('Y-m-d H:i:s', strtotime('-' . $this->config['article_sales_class_history'] . ' days')))
                 ->first();
 
             $articleData[] = [
@@ -83,10 +100,30 @@ class ClassifyArticles extends Command
             ];
         }
 
-        // Sort articles by sales volume
-        usort($articleData, function ($a, $b) {
-            return $a['unique_invoices'] < $b['unique_invoices'];
-        });
+        // Sort articles based on calculation type
+        switch ($this->config['sales_class_calculation']) {
+            case 'both':
+                usort($articleData, function ($a, $b) {
+                    $aScore = ($a['unique_invoices'] * 10) + $a['sales_volume'];
+                    $bScore = ($b['unique_invoices'] * 10) + $b['sales_volume'];
+
+                    return $aScore < $bScore;
+                });
+                break;
+
+            case 'invoices':
+                usort($articleData, function ($a, $b) {
+                    return $a['unique_invoices'] < $b['unique_invoices'];
+                });
+                break;
+
+            case 'sales':
+            default:
+                usort($articleData, function ($a, $b) {
+                    return $a['sales_volume'] < $b['sales_volume'];
+                });
+                break;
+        }
 
         $classCounts = [
             'A' => 0,
@@ -97,8 +134,8 @@ class ClassifyArticles extends Command
         for ($i = 1;$i <= count($articleData);$i++) {
             $article = $articleData[$i - 1];
 
-            if ($classCounts['A'] < 35) {
-                if ($article['unique_invoices'] >= 3) {
+            if ($classCounts['A'] < $this->config['article_sales_class_size_a']) {
+                if ($article['unique_invoices'] >= $this->config['article_sales_class_unique_invoices']) {
                     $classification = 'A';
                     $classCounts['A']++;
                 }
@@ -107,7 +144,7 @@ class ClassifyArticles extends Command
                     $classCounts['B']++;
                 }
             }
-            else if ($classCounts['B'] < 150) {
+            else if ($classCounts['B'] < $this->config['article_sales_class_size_b']) {
                 $classification = 'B';
                 $classCounts['B']++;
             }
