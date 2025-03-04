0e8f6552514ad8c02fd5457f26c2686c07c420b4
Anton Kihlström <anton@scriptsector.se>
2025-03-04 13:01:58 +0100

    Fix loop issue

--- app/Services/WMS/StockOptimizationManager.php
+++ app/Services/WMS/StockOptimizationManager.php
@@ -196,6 +196,8 @@ class StockOptimizationManager
                                 }
                             }
                         }
+
+                        $toplistIndex++;
                     }
                 }
             }
