417617cd5de455548f5b536add5865bc11e4888c
Anton Kihlström <anton@scriptsector.se>
2025-05-02 14:12:58 +0200

    Temporary skip order create dispatch

--- app/Services/SalesOrderService.php
+++ app/Services/SalesOrderService.php
@@ -249,7 +249,7 @@ class SalesOrderService
 
         $skipDispatch = $data['skip_dispatch'] ?? false;
         if (!$skipDispatch) {
-            (new DispatchOrderCreated())->execute($salesOrder);
+            // (new DispatchOrderCreated())->execute($salesOrder);
         }
 
         $skipEmail = $data['skip_email'] ?? false;
