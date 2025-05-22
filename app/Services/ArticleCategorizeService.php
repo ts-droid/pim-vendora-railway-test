2a87c37d174287d2b4217a3676898056e34dbed9
Anton Kihlström <anton@scriptsector.se>
2025-05-22 17:35:15 +0200

    Suppli categories to ai

--- app/Services/ArticleCategorizeService.php
+++ app/Services/ArticleCategorizeService.php
@@ -7,14 +7,17 @@ use App\Services\AI\AIService;
 
 class ArticleCategorizeService
 {
-    const MODEL = 'gpt-4o';
+    const MODEL = 'gpt-4.1-nano-2025-04-14';
 
     public function categorizeArticle(Article $article)
     {
-        $system = 'Product description:' . PHP_EOL . ($article->shop_description_en ?? '');
+        $path = storage_path('google_product_categories.txt');
+        $googleCategories = file_get_contents($path);
+
+        $system = 'Product description:' . PHP_EOL . ($article->shop_description_en ?? '' . PHP_EOL . PHP_EOL . 'Google Product Categories:' . PHP_EOL . $googleCategories);
 
         $message = 'You are a e-commerce assistant. Read through the product description and give me the most relevant Google Product Category.
-        Respond only with the category name. The category name must be in English.';
+        Respond only with the category id.';
 
         $AIService = new AiService(self::MODEL);
         $response = $AIService->chatCompletion($system, $message);
