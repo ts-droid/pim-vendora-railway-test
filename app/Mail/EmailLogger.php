eddc4bb37cfc7d3194d176360e1a81742be7ff18
Anton Kihlström <anton@scriptsector.se>
2025-06-25 10:47:06 +0200

    Fix for EmailLogger

--- app/Mail/EmailLogger.php
+++ app/Mail/EmailLogger.php
@@ -9,12 +9,24 @@ use Symfony\Component\Mime\Email as SymfonyEmail;
 
 class EmailLogger
 {
-    public static function log(string $to, ?string $cc, ?string $bcc, string $subject, string $body, ?array $attachments): Email
+    public static function log(string|array $to, string|array|null $cc, string|array|null $bcc, string $subject, string $body, ?array $attachments): Email
     {
+        if (is_array($to)) {
+            $to = implode(',', $to);
+        }
+
+        if (is_array($cc)) {
+            $cc = implode(',', $cc);
+        }
+
+        if (is_array($bcc)) {
+            $bcc = implode(',', $bcc);
+        }
+
         return Email::create([
-            'to' => $to,
-            'cc' => $cc,
-            'bcc' => $bcc,
+            'to' => (string) $to,
+            'cc' => (string) $cc,
+            'bcc' => (string) $bcc,
             'subject' => $subject,
             'body' => $body,
             'attachments' => $attachments,
