0f0e49eeb06fb2642b38837e01e836335bc1293b
Anton Kihlström <anton@scriptsector.se>
2025-07-28 08:43:47 +0200

    Move route

--- routes/api.php
+++ routes/api.php
@@ -59,11 +59,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('/v2')->middleware(['api.key', 'gzip'])->group(function() {
     Route::prefix('/articles')->group(function() {
-        Route::post('/', [ArticleController::class, 'storeV2'])->name('articles.store.v2');
-        Route::post('/{article}', [ArticleController::class, 'updateV2'])->name('articles.update.v2');
-
         Route::get('/customer-reviews', [ArticleController::class, 'customerReviews'])->name('articles.customerReviews');
         Route::post('/customer-reviews', [ArticleController::class, 'customerReviewsStore'])->name('articles.customerReviews.store');
+
+        Route::post('/', [ArticleController::class, 'storeV2'])->name('articles.store.v2');
+        Route::post('/{article}', [ArticleController::class, 'updateV2'])->name('articles.update.v2');
     });
 });
 
