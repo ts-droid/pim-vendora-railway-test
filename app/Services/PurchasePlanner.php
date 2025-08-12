f02367bf48162008b7a60b823ee0789068aa5c37
Anton Kihlström <anton@scriptsector.se>
2025-08-12 18:57:48 +0200

    Disable logs

--- app/Services/PurchasePlanner.php
+++ app/Services/PurchasePlanner.php
@@ -16,8 +16,8 @@ use Illuminate\Support\Facades\Storage;
 
 class PurchasePlanner
 {
-    private const LOG_DATA = true;
-    private const LOG_ARTICLES = ['PL2A-11-18'];
+    private const LOG_DATA = false;
+    private const LOG_ARTICLES = [];
 
 	private const ZSCORE_LIMIT = 3.0;
 	private const TRIM_PERCENT = 0.1; // 10% over/under
