03aa381c78493111d94339c49f71c0c471db4eaf
Anton Kihlström <anton@scriptsector.se>
2025-08-13 07:40:12 +0200

    Add logging

--- app/Services/PurchasePlanner.php
+++ app/Services/PurchasePlanner.php
@@ -16,8 +16,8 @@ use Illuminate\Support\Facades\Storage;
 
 class PurchasePlanner
 {
-    private const LOG_DATA = false;
-    private const LOG_ARTICLES = [];
+    private const LOG_DATA = true;
+    private const LOG_ARTICLES = ['12-2010'];
 
 	private const ZSCORE_LIMIT = 3.0;
 	private const TRIM_PERCENT = 0.0; // 0.1 = 10% over/under
