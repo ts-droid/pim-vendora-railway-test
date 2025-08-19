49cc3b92160a55883f1e854649cda3c4bd6c3993
Anton Kihlström <anton@scriptsector.se>
2025-08-19 20:28:58 +0200

    Fix getNotShippedValue()

--- app/Models/PurchaseOrder.php
+++ app/Models/PurchaseOrder.php
@@ -162,7 +162,7 @@ class PurchaseOrder extends Model
         $total = 0;
 
         foreach ($this->lines as $line) {
-            $total += $line->is_shipped ? 0 : ($line->quantity * $line->unit_cost);
+            $total += ($line->is_shipped || $line->is_completed) ? 0 : ($line->quantity * $line->unit_cost);
         }
 
         return $total;
