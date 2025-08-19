7c6e0e915f81f2cbc84e37efd5d60ffaae65402c
Anton Kihlström <anton@scriptsector.se>
2025-08-19 15:24:47 +0200

    Add link to reports

--- resources/views/supplierPortal/header.blade.php
+++ resources/views/supplierPortal/header.blade.php
@@ -6,7 +6,10 @@
     </div>
     <div class="header__links">
         <div class="header__link">
-            <a href="{{ route('supplierPortal.purchaseOrders.index') }}">Purhcase Orders</a>
+            <a href="{{ route('supplierPortal.purchaseOrders.index') }}">Purchase Orders</a>
+        </div>
+        <div class="header__link">
+            <a href="https://adm.vendora.se/" target="_blank">Reports</a>
         </div>
     </div>
     <div class="header__text">Desktop Version</div>
