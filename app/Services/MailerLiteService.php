61bcef00128fbe375e1adb8c07cfc8700d9067db
Anton Kihlström <anton@scriptsector.se>
2025-11-28 12:24:13 +0100

    addSubscriber

--- app/Services/MailerLiteService.php
+++ app/Services/MailerLiteService.php
@@ -17,8 +17,35 @@ class MailerLiteService
         $this->mailerLite = new MailerLite(['api_key' => $this->token]);
     }
 
-    public function addSubscriber(string $email)
+    public function addSubscriber(string $email, ?string $groupName = null): ?array
     {
         $response = $this->mailerLite->subscribers->create(['email' => $email]);
+        $subscriber = $response['body']['data'] ?? null;
+
+        if (!$subscriber) return null;
+
+        if ($groupName) {
+            $group = $this->getGroupByName($groupName);
+            if ($group) {
+                $this->mailerLite->groups->assignSubscriber($group['id'], $subscriber['id']);
+            }
+        }
+
+        return $subscriber;
+    }
+
+    public function getGroupByName(string $groupName): ?array
+    {
+        $response = $this->mailerLite->groups->get();
+        $groups = $response['body']['data'] ?? [];
+
+        foreach ($groups as $group) {
+            if ($group['name'] != $groupName) continue;
+
+            return $group;
+        }
+
+        $response = $this->mailerLite->groups->create(['name' => $groupName]);
+        return $response['body']['data'] ?? null;
     }
 }
