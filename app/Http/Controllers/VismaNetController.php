4f9d212a6ff487f07998e77ccae4761a4a73554b
Anton Kihlström <anton@scriptsector.se>
2026-01-22 18:08:46 +0100

    Fetch also hold orders

--- app/Http/Controllers/VismaNetController.php
+++ app/Http/Controllers/VismaNetController.php
@@ -355,10 +355,6 @@ class VismaNetController extends Controller
             foreach ($orders as $order) {
                 $fetchedData = true;
 
-                if ($order['hold'] ?? false) {
-                    continue;
-                }
-
                 $promisedOn = $order['promisedOn'] ?? '';
                 $promisedDate = $promisedOn ? (date('Y-m-d', strtotime($promisedOn))) : '';
 
