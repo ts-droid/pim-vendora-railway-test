b99fb100960f1968ba8a48c9cc0ca160125fc25e
Anton Kihlström <anton@scriptsector.se>
2026-03-30 07:50:57 +0200

    Enable translate database again

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
             $schedule->command('shipments:postpone-eta')->dailyAt('01:00');
 
             // TODO: Can me moved to long loved processes
-            // $schedule->command('translate-database')->everyFiveMinutes()->withoutOverlapping(); // TODO: Enable this after debugging
+            $schedule->command('translate-database')->everyFiveMinutes()->withoutOverlapping();
             $schedule->command('servers:monitor')->everyTenMinutes()->withoutOverlapping();
             $schedule->command('visma:process-queue')->everyMinute()->withoutOverlapping();
 
