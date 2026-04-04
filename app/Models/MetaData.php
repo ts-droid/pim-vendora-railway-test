480078a33c20e29389f6d5dea6b82e73996dcb25
Anton Kihlström <anton@scriptsector.se>
2026-04-04 11:05:31 +0200

    Update

--- app/Models/MetaData.php
+++ app/Models/MetaData.php
@@ -9,6 +9,18 @@ class MetaData extends Model
 {
     use HasFactory;
 
+    /**
+     * The meta_data table uses `name` as its primary key.
+     */
+    protected $primaryKey = 'name';
+
+    /**
+     * `name` is a string key and it does not auto-increment.
+     */
+    public $incrementing = false;
+
+    protected $keyType = 'string';
+
     protected $fillable = [
         'name',
         'value'
