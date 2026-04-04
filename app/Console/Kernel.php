73d33dbb71e5782871b3ccf15a014893d34df231
Anton Kihlström <anton@scriptsector.se>
2026-04-04 13:14:19 +0200

    Enable translations

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
 
             $schedule->command('shipments:postpone-eta')->dailyAt('01:00');
 
-            // $schedule->command('translate-database')->everyFifteenMinutes()->withoutOverlapping(120);
+            $schedule->command('translate-database')->everyFifteenMinutes()->withoutOverlapping(120);
             $schedule->command('faq:generate-missing')->everyFifteenMinutes()->withoutOverlapping(360);
 
             // TODO: Can me moved to long loved processes
